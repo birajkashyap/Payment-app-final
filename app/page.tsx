5727ebd51f17b7b02cb997b66546d0b576967965
Biraj Kashyap <kashyapbiraj83@gmail.com>
2025-07-16 23:56:58 +0530

    ai-interviewer setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Link } from "next/link";
+
+import { Button } from "@/components/ui/button";
+const Page = () => {
+  return (
+    <div>
+      <div className="text-2xl underline">Page</div>;<Button></Button>
+    </div>
+  );
+};
+
+export default Page;
